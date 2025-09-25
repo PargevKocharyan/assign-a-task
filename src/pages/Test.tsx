6fee2b8071df5930ec57ae33008d09979b822b38
Appassion <126763377+PargevKocharyan@users.noreply.github.com>
2025-09-25 17:41:09 +0300

    Update Test.tsx

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,7 +1,7 @@
 const Test = () => {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">hello</h1>
+      <h1 className="text-4xl font-bold">hello!!</h1>
     </main>
   );
 };
